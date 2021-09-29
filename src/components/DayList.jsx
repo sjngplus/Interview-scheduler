d63c6bfb398d595c7b77fb56124bd541f475014c
SJ <sj.jega2@gmail.com>
2021-09-28 23:40:14 -0400

    Add built components to App.js

--- src/components/DayList.jsx
+++ src/components/DayList.jsx
@@ -1,23 +1,6 @@
 import React from "react";
 import DayListItem from "./DayListItem"
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 export default function DayList(props) {
   const parsedDays = props.days.map((day) => {
